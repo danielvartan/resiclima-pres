4edacc808c95b9faf204447a1e92ec39515613b7
Daniel Vartanian <danvartan@gmail.com>
2024-08-25 20:18:20 -0300

    Update quarto-setup.R

--- R/quarto-setup.R
+++ R/quarto-setup.R
@@ -42,9 +42,11 @@ ggplot2::theme_set(
 
 # Run `rbbt` -----
 
-# (2024-06-05)
-# This function only works with BetterBibTeX (BBT) for Zotero version v6.7.140
-# or lower.
+# (2024-08-25)
+# This function should work with any version of BetterBibTeX (BBT) for Zotero.
+# Verify if @wmoldham PR was merged in the `rbbt` package (see issue #47
+# <https://github.com/paleolimbot/rbbt/issues/47>). If not, install `rbbt`
+# from @wmoldham fork `remotes::install_github("wmoldham/rbbt")`.
 
 rutils:::bbt_write_quarto_bib(
   bib_file = here::here("references.bib"),
